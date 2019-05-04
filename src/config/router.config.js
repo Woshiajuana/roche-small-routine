0769ede17ad0d13d9b3b9c9e1c2b1d83ddf2fb34
Woshiajuana <zhigang.chen@owulia.com>
2019-05-04 18:23:50 +0800

    submit code

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     "activation_index": "/pages/activation/index",
+    "home_index": "/pages/home/index",
     "login_index": "/pages/login/index",
+    "mine_index": "/pages/mine/index",
     "questionnaire_answerone_index": "/pages/questionnaire/answerone/index",
     "questionnaire_answerthree_index": "/pages/questionnaire/answerthree/index",
     "questionnaire_answertwo_index": "/pages/questionnaire/answertwo/index",
