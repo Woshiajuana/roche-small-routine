bf465beb28b66c0a12b7e7799a24c14c4aab28e4
Woshiajuana <zhigang.chen@owulia.com>
2019-05-06 14:05:08 +0800

    tijiao

--- src/pages/clock/index.js
+++ src/pages/clock/index.js
@@ -7,8 +7,6 @@ import Http                         from 'plugins/http.plugin'
 import Router                       from 'plugins/router.plugin'
 import Modal                        from 'plugins/modal.plugin'
 import Mixin                        from 'utils/mixin.util'
-import Valid                        from 'utils/valid.util'
-import InputMixin                   from 'mixins/input.mixin'
 import SourceMixin                  from 'mixins/source.mixin'
 import UserMixin                    from 'mixins/user.mixin'
 import DataMixin                    from './data.mixin'
@@ -21,34 +19,25 @@ Page(Mixin({
     mixins: [
         UserMixin,
         DataMixin,
-        InputMixin,
         SourceMixin,
     ],
     onLoad () {
-        this.userGet();
         this.sourceGet(arrSrc);
+        this.userGet();
+        this.drawCircle();
     },
-    inputCallback () {
-        let { formData, arrData } = this.data;
-        let Code = formData.Code.value;
-        Code = Code.split('');
-        let arr = ['', '', '', '', '', ''];
-        arrData = [...Code, ...arr];
-        arrData = arrData.slice(0, 6);
-        this.setData({arrData});
-    },
-    // 购买会员
-    handleSubmit () {
-        if (Valid.check(this.data.formData)) return null;
-        let data = Valid.input(this.data.formData);
-        return Http(Http.API.Do_setMemberInfo, data).then((res) => {
-            Modal.confirm({
-                content: '恭喜您，您的服务已开通！',
-                showCancel: false,
-                confirmText: '返回首页',
-            }).then(() => {
-                Router.root('home_index');
-            });
-        }).toast();
+    drawCircle (step){
+        let context = wx.createCanvasContext('canvasProgress');
+        let gradient = context.createLinearGradient(200, 100, 100, 200);
+        gradient.addColorStop("0", "#2661DD");
+        gradient.addColorStop("0.5", "#40ED94");
+        gradient.addColorStop("1.0", "#5956CC");
+        context.setLineWidth(10);
+        context.setStrokeStyle(gradient);
+        context.setLineCap('round');
+        context.beginPath();
+        context.arc(110, 110, 100, -Math.PI / 2, step * Math.PI - Math.PI / 2, false);
+        context.stroke();
+        context.draw()
     },
 }));
