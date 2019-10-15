072449e769073f3e8ffcf70d719e02763b0eae9b
Woshiajuana <zhigang.chen@owulia.com>
2019-10-15 11:35:44 +0800

    subcode

--- src/config/env.sc.config.js
+++ src/config/env.sc.config.js
@@ -1,4 +1,4 @@
 export default {
-    IMAGE_URL: 'https://api.jiappo.cn/static/images/',             // 静态资源服务器
+    IMAGE_URL: 'https://www.sugarmini.com/static/images/',             // 静态资源服务器
     API_URL: 'https://www.sugarmini.com/',                                  // 接口
 }
