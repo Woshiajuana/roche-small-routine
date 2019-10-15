a53b1882d36ec5a02a38d6191a11e748f1206d23
Woshiajuana <zhigang.chen@owulia.com>
2019-10-15 14:07:02 +0800

    subcode share.mixin.js

--- src/mixins/share.mixin.js
+++ src/mixins/share.mixin.js
@@ -4,7 +4,7 @@ export default {
             title: '360° 稳糖管家',
             path: '/pages/home/index',
             // imageUrl: 'http://mmbiz.qpic.cn/mmbiz_jpg/7P9iboeqZDgsWxZ2yvqLP8nYxwicib4ln1KTZmh1EfEmNKrP890afVkvglCoqvm5lxhTZvZKeA2xH8HjYEWLUhPlQ/0?wx_fmt=jpeg',
-            imageUrl: 'https://api.jiappo.cn/static/images/519122441.jpg',
+            imageUrl: 'https://www.sugarmini.com/static/images/519122441.jpg',
         }
     },
 }
