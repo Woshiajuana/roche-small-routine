c13c861be0f203247e73b67a77d94930d06fef22
Woshiajuana <zhigang.chen@owulia.com>
2019-11-28 10:28:29 +0800

    subcode

--- src/pages/bluetooth/synchronization/index.js
+++ src/pages/bluetooth/synchronization/index.js
@@ -8,6 +8,10 @@ import SyncMixin                    from 'mixins/sync-data.mixin'
 import RouterMixin                  from 'mixins/router.mixin'
 import SourceMixin                  from 'mixins/source.mixin'
 import BleMixin                     from 'mixins/ble.mixin'
+import Modal                        from 'plugins/modal.plugin'
+import Http                         from 'plugins/http.plugin'
+import Loading                      from 'plugins/loading.plugin'
+import Authorize                    from 'plugins/authorize.plugin'
 
 const arrSrc = [
     { key: 'bg', value: 'sjtb-explain-bg.jpg' },
@@ -34,14 +38,24 @@ Page(Mixin({
         });
     },
     handleSync1 () {
-        let { deviceId, serviceId } = this.data.params$;
-        this.bleSyncData(deviceId, serviceId, (res) => {
-            console.log('成功1', res);
-        },(res) => {
-            console.log('成功2', res);
-        },(err) => {
-            console.log('失败', err);
+        if (this.data.tapType) return;
+        this.setData({tapType: true});
+        Authorize(Authorize.SCOPE.userLocation, '同步数据需要地理位置授权').then(() => {
+            let { deviceId, serviceId } = this.data.params$;
+            this.bleSyncData(deviceId, serviceId, (res) => {
+                console.log('成功1', res);
+            },(res) => {
+                console.log('成功2', res);
+            },(err) => {
+                console.log('失败', err);
+            });
+        }).catch((err) => {
+            this.setData({tapType: false});
+            let { errCode } = err;
+            if (errCode === -999) return Modal.toast('您还未配对过设备，请先去配对设备');
+            Modal.toast('同步数据需要地理位置授权哦')
         });
+
     },
 
 }));
