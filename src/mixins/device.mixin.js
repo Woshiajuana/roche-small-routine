0ce889871b98cd72ad0849daa0dea32fd5e18802
Woshiajuana <zhigang.chen@owulia.com>
2020-01-17 18:21:43 +0800

    subcode code

---
+++ src/mixins/device.mixin.js
@@ -0,0 +1,19 @@
+
+import Source                       from 'utils/source.util'
+
+
+export default {
+    data: {
+        arrDevice: [
+            {
+                selectSrc: Source('20200117-s-1.png'),
+            },
+            {
+                selectSrc: Source('20200117-s-2.png'),
+            },
+            {
+                selectSrc: Source('20200117-s-3.png'),
+            },
+        ],
+    },
+}
