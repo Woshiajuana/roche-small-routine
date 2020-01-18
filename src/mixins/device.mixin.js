af5d221deb656668612f57ee34494f785895e02e
Woshiajuana <zhigang.chen@owulia.com>
2020-01-18 20:20:02 +0800

    subcode

--- src/mixins/device.mixin.js
+++ src/mixins/device.mixin.js
@@ -7,17 +7,17 @@ export default {
             {
                 selectSrc: Source('20200117-s-1.png'),
                 explainSrc: Source('20200117-e-1.jpg'),
-                explainText: '罗氏 血糖仪Performa Connect',
+                explainText: '罗氏®血糖仪Performa Connect',
             },
             {
                 selectSrc: Source('20200117-s-2.png'),
                 explainSrc: Source('20200117-e-2.jpg'),
-                explainText: '罗氏 血糖仪Performa Connect',
+                explainText: '罗氏®智航®Guide',
             },
             {
                 selectSrc: Source('20200117-s-3.png'),
                 explainSrc: Source('20200117-e-3.jpg'),
-                explainText: '罗氏 血糖仪Performa Connect',
+                explainText: '罗氏®逸智®Instant',
             },
         ],
     },
