ba6106f8333bb306681c3bc6f6c2f366830c90ba
Woshiajuana <zhigang.chen@owulia.com>
2020-04-08 09:08:06 +0800

    subcode code

--- src/config/env.bd.config.js
+++ src/config/env.bd.config.js
@@ -1,7 +1,7 @@
 import BD_IP                    from './../../config/ip.config'
 export default {
-    IMAGE_URL: 'http://'+ BD_IP +':10011/static/images/',             // 静态资源服务器
-    // IMAGE_URL: 'https://www.sugarmini.com/static/images/',             // 静态资源服务器
+    // IMAGE_URL: 'http://'+ BD_IP +':10011/static/images/',             // 静态资源服务器
+    IMAGE_URL: 'https://www.sugarmini.com/static/images/v1/',             // 静态资源服务器
     // IMAGE_URL: 'https://api.jiappo.cn/static/images/',             // 静态资源服务器
     // API_URL: 'https://api.jiappo.cn/',         // 接口
     API_URL: 'https://www.sugarmini.com/',                                  // 接口
