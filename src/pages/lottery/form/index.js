a54e02a7dd132795484f8be95ed09f16df6f9a3b
Woshiajuana <zhigang.chen@owulia.com>
2020-05-17 19:31:37 +0800

    subcode code

--- src/pages/lottery/form/index.js
+++ src/pages/lottery/form/index.js
@@ -5,29 +5,16 @@ import './index.wxml'
 
 import Router                       from 'plugins/router.plugin'
 import Mixin                        from 'utils/mixin.util'
-import SourceMixin                  from 'mixins/source.mixin'
 import UserMixin                    from 'mixins/user.mixin'
-
-const arrSrc = [
-    { key: 'bg', value: 'luck-draw-bg.jpg' },
-];
 
 Page(Mixin({
     mixins: [
         UserMixin,
-        SourceMixin,
     ],
     data: {
-        isPopup: false,
+
     },
     onLoad () {
-        this.sourceGet(arrSrc);
         this.userGet();
     },
-    handleJump () {
-        this.setData({isPopup: true});
-    },
-    handleRoot () {
-        Router.root('home_index');
-    }
 }));
