06d33d935442b3fb5423c05658d4ffb984354ab7
Woshiajuana <zhigang.chen@owulia.com>
2020-06-23 14:14:26 +0800

    subcode code

--- src/pages/lottery/index.js
+++ src/pages/lottery/index.js
@@ -19,31 +19,22 @@ Page(Mixin({
     },
     onLoad () {
         this.userGet();
+        Http(Http.API.Req_GetActivitySwitch).then((res) => {
+            this.setData({ isTimePop: !res })
+        }).toast();
     },
     // 打卡记录
     handleJumpRecord () {
-        Http(Http.API.Req_GetActivitySwitch).then((res) => {
-            if (res) {
-                Router.push('lottery_record_index');
-            } else {
-                this.setData({ isTimePop: true })
-            }
-        }).toast();
+        Router.push('lottery_record_index');
     },
     // 立即打卡
     handleJumpClockIn () {
-        Http(Http.API.Req_GetActivitySwitch).then((res) => {
-            if (res) {
-                Http(Http.API.Req_GetActivityUser).then((res) => {
-                    let { IsGuide } = res;
-                    Router.push(
-                        IsGuide ? 'bluetooth_synchronization_index' : 'bluetooth_explain_index',
-                        { index: 1, result: true, from: 'lottery_index' }
-                    );
-                }).toast();
-            } else {
-                this.setData({ isTimePop: true })
-            }
+        Http(Http.API.Req_GetActivityUser).then((res) => {
+            let { IsGuide } = res;
+            Router.push(
+                IsGuide ? 'bluetooth_synchronization_index' : 'bluetooth_explain_index',
+                { index: 1, result: true, from: 'lottery_index' }
+            );
         }).toast();
     },
     handlePopup (event) {
